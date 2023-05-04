6bb06af27ba3d6d829d0fa14b147f92fd2d97643
Anant Deshmukh <anant7564@gmail.com>
2023-05-04 21:19:30 +0530

    Dashboard created

--- website/src/pages/Dashboard.jsx
+++ website/src/pages/Dashboard.jsx
@@ -1,21 +1,43 @@
+import { useEffect, useState } from "react";
 import isLoggedIn from "../components/isLoggedIn";
-
+import firebase from "firebase/compat/app";
+import firebaseConfig from "../components/config/firebaseConfig";
+import "firebase/compat/database"
 export default function Dashboard() {
-  
+
   const user = localStorage.getItem("user");
   const role = localStorage.getItem("role");
   const dept = localStorage.getItem("dept");
+  const [name, setName] = useState();
 
-  const isUserLoggedIn = isLoggedIn();
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig)
 
-  if (!isUserLoggedIn) {
-    window.location.href = "/";
-    return null;
-  }
+    const isUserLoggedIn = isLoggedIn();
+    if (!isUserLoggedIn) {
+      window.location.href = "/";
+      return null;
+    }
+    const userRef = firebase.database().ref("Departments").child(dept).child(role).child(user);
+    const userData = userRef.on("value", snapshot => {
+      setName(snapshot.val().name);
+    }
+    );
+
+  }, [])
+
+
 
   return (
-    <div className="dashItem">
-      This is the dashboard page for {user} with role {role} and dept {dept}
+    <div className="container my-4 px-5 py-4 rounded shadow bg-body-tertiary">
+      <h2 className="mb-4">Welcome, {name} </h2>
+      <div>
+        <h4>ASHE Report</h4>
+        <p style={{ textAlign: "justify" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium eius a omnis impedit at quam, obcaecati eveniet. Quidem, quo alias. Doloribus molestias aspernatur commodi similique placeat dignissimos molestiae aliquid facilis autem. Suscipit quas quidem itaque veniam ipsa nam labore, quod corporis error asperiores tenetur vero unde officiis numquam modi, dicta libero dolorum quos maiores quam iste ea excepturi? Voluptatibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Non rerum ad consequuntur! Eaque, magni repellendus sit, similique veniam aut omnis sequi vitae ea distinctio delectus maxime temporibus rem voluptatum corrupti.</p>
+      </div>
+      <div className="text-end">
+        <button type="button" class="btn btn-outline-success">Generate Report</button>
+      </div>
     </div>
   );
 }
